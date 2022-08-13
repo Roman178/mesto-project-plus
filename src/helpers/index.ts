728968652df946e1f6b4a34fef7e1e575aa2bc2c
Roman <ismagilov771@gmail.com>
2022-08-14 00:00:53 +0300

    review fixes

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -10,18 +10,9 @@ const errorResponse = (message: string): IErrorResponse => ({
   message,
 });
 
-const getInvalidFields = (message: string): string => {
-  const invalidFields: string[] = [];
-  message.split(' ').forEach((word: string, i: number, arr: string[]) => {
-    if (word === 'Path') invalidFields.push(arr[i + 1]);
-  });
-
-  return invalidFields.join(', ');
-};
-
 const urlRegexp = /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/; // eslint-disable-line
 const urlValidator = (value: string) => urlRegexp.test(value);
 
 export {
-  successResponse, errorResponse, getInvalidFields, urlValidator, urlRegexp,
+  successResponse, errorResponse, urlValidator, urlRegexp,
 };
